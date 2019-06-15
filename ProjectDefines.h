3ae30b27218bb619a1044178882b919686ca0575
bolke <debolke@gmail.com>
2019-06-15 23:27:46 +0200

    updated project for test

--- ProjectDefines.h
+++ ProjectDefines.h
@@ -1,10 +1,10 @@
 #ifndef PROJECTDEFINES_H                                                                //
 #define PROJECTDEFINES_H                                                                //
                                                                                         //
-  #define VDATE                         "Jun 03 2019"                                   //build date, used as version, format "MMM DD YYYY"
-  #define VTIME                         "12:12:12"                                      //build time, used as version, format "hh:mm:ss"
+  #define VDATE                         "Jun 15 2019"                                   //build date, used as version, format "MMM DD YYYY"
+  #define VTIME                         "21:39:12"                                      //build time, used as version, format "hh:mm:ss"
                                                                                         //
-  #define PROJECT_ID                    "01"                                            //unique project identifier, format base64 "xx"
+  #define PROJECT_ID                    "00"                                            //unique project identifier, format base64 "xx"
   #define MAGIC_NUMBER                  0xC369                                          //eeprom value used to check if eeprom is set
   #define MAGIC_ADDRESS                 0x00                                            //eeprom address to read the magic number from
                                                                                         //
@@ -15,7 +15,7 @@
   #define GPIO_DEFAULT                  GPIO_INPUT_HIGH                                 //default value of unused gpio
                                                                                         //
   #define GPIO_INPUT_LOW                0x01                                            //input low value first declared, as it is default for input
-  #define GPIO_INPUT                    GPIO_INPUT_LOW                                  //value for gpio input default  
+  #define GPIO_INPUT                    GPIO_INPUT_LOW                                  //default value for gpio input   
   #define GPIO_INPUT_HIGH               0x02                                            //
   #define GPIO_OUTPUT_LOW               0x03                                            //
   #define GPIO_OUTPUT                   GPIO_OUTPUT_LOW                                 //
