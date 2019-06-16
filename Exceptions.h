ee16596996d6f883580aa250a13685110c732fd4
bolke <debolke@gmail.com>
2019-06-16 14:12:03 +0200

    spaced

--- Exceptions.h
+++ Exceptions.h
@@ -5,7 +5,7 @@
     #define EXCEPTION_INVALID_FUNCTION 0x01                                             //invalid function code in request
     #define EXCEPTION_INVALID_ADDRESS 0x02                                              //invalid address in request
     #define EXCEPTION_INVALID_VALUE 0x03                                                //invalid value in request, depends upon value usage
-    #define EXCEPTION_SLAVE_DEVICE_FAILURE 0x04                                         //failure when executing request
+    #define EXCEPTION_DEVICE_FAILURE 0x04                                               //failure when executing request
     #define EXCEPTION_ACKNOWLEDGE 0x05                                                  //received request, couldn't finish on time
     #define EXCEPTION_SLAVE_DEVICE_BUSY 0x06                                            //can't execute request at the moment
     #define EXCEPTION_NEGATIVE_ACKNOWLEDGE 0x07                                         //can't acknowledge request
